ad225e31f7804005af56f019cc99376e099f8e87
Robin Denton <raenton@gmail.com>
2020-05-08 16:24:49 +0100

    Refactored user qquery method to use new auth util

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -5,9 +5,7 @@ exports.users = (_parent, _args, context) => {
 exports.user = async (_parent, args, context) => {
   const { auth, prisma } = context
   const { id, username, email } = await prisma.user({ id: args.id })
-
-  const userId = auth.getUserId(context)
-  const isCurrentUser = (userId && userId === args.id)
+  const isCurrentUser = auth.isCurrentUser(context, args.id)
   return {
     id,
     username,
