23510711b861154091c9aa1991482f6741e6341f
Robin Denton <raenton@gmail.com>
2020-05-12 19:10:56 +0100

    Refactor posts pagination to work with prisma

--- src/resolvers/queries/post.query.js
+++ src/resolvers/queries/post.query.js
@@ -1,33 +1,9 @@
 exports.posts = async (_parent, args, context) => {
-  const { utils, prisma } = context
-  const { first, after } = args.paginationInput
-
-  const where = after ? {
-    createdAt_lt: utils.fromCursorHash(after)
-  } : {}
-
-  const posts = await prisma.posts({
-    where: where,
-    first: first + 1,
-    orderBy: 'createdAt_DESC'
+  const postsConnection = await context.prisma.postsConnection({
+    ...args.paginationInput
   })
-
-  const hasNextPage = posts.length > first
-  
-  const nodes = hasNextPage
-    ? posts.slice(0, -1)
-    : posts
-
-  const edges = nodes.map(node => ({ node }))
-  const endCursor = utils.toCursorHash(
-    edges[edges.length - 1].node.createdAt.toString()
-  )
-
   return {
-    count: edges.length,
-    edges: edges,
-    pageInfo: {
-      hasNextPage, endCursor
-    }
+    count: postsConnection.edges.length,
+    ...postsConnection
   }
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,3 +2,36 @@ exports.toCursorHash = (string) => Buffer.from(string).toString('base64');
 
 exports.fromCursorHash = (string) =>
   Buffer.from(string, 'base64').toString('ascii');
+
+// exports.paginate = async (where, args, context, type) => {
+//   const { first, after } = args
+
+//   if (after) {
+//     where['createdAt_lt'] = this.fromCursorHash(after)
+//   }
+
+//   const data = await context.prisma[type]({
+//     where: where,
+//     first: first + 1,
+//     orderBy: 'createdAt_DESC'
+//   })
+
+//   const hasNextPage = data.length > first
+  
+//   const nodes = hasNextPage
+//     ? data.slice(0, -1)
+//     : data
+
+//   const edges = nodes.map(node => ({ node }))
+//   const endCursor = this.toCursorHash(
+//     edges[edges.length - 1].node.createdAt.toString()
+//   )
+
+//   return {
+//     count: edges.length,
+//     edges: edges,
+//     pageInfo: {
+//       hasNextPage, endCursor
+//     }
+//   }
+// }
