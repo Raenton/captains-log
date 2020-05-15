2f4882849615133c4c40e021e034779aa065f91c
Robin Denton <raenton@gmail.com>
2020-05-15 19:32:15 +0100

    config js requires env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  SECRET: 'SHOULD_BE_IN_ENV'
-}
+const path = require('path')
+
+const envFile = process.env.NODE_ENV === 'development'
+  ? path.resolve(process.cwd(), '.env.development')
+  : path.resolve(process.cwd(), '.env')
+
+require('dotenv').config({
+  path: envFile
+})
+
+module.exports = process.env
