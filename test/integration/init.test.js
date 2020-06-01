38d078f182b852befd3a534fa43143d8fa657bba
Robin Denton <raenton@gmail.com>
2020-06-01 09:44:05 +0100

    Remove extraneous test db modification from integration tests initialisation

--- test/integration/init.test.js
+++ test/integration/init.test.js
@@ -1,12 +1,5 @@
 const path = require('path')
-const dbHelper = require('./helpers/dbHelper')
 
 require('dotenv').config({
   path: path.resolve(process.cwd(), '.env.test')
-})
-
-dbHelper.findOrCreateUser({
-  username: 'test_user',
-  email: 'test@mail.com',
-  password: 'test123'
 })
